223852421792c48a7186ec6691a11bee302c7e23
hyun6ik <fkanwm@naver.com>
2021-03-01 21:03:11 +0900

    프로젝트 생성

---
+++ spring_login_register_email/src/main/java/com/example/spring_login_register_email/SpringLoginRegisterEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_login_register_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginRegisterEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginRegisterEmailApplication.class, args);
+	}
+
+}
