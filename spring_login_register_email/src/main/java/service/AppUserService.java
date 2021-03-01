7b8b8b5223c4aa8c99b2714e67c936b81118d6e5
hyun6ik <fkanwm@naver.com>
2021-03-01 21:36:24 +0900

    AppUser 엔티티,서비스,리포지토리 UserDetails, UserDetailsService SimpleGrantedAutority 사용

---
+++ spring_login_register_email/src/main/java/service/AppUserService.java
@@ -0,0 +1,24 @@
+package service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import repository.AppUserRepository;
+
+
+@Service
+@RequiredArgsConstructor
+public class AppUserService  implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND_MSG = "%s로 된 이메일을 찾을 수 없습니다";
+    private final AppUserRepository appUserRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+}
